d336eeedaca513a3b202764a1e7b16c2bb9c4042
ravinderreddy-p <ravinder9.reddy@gmail.com>
2018-11-18 01:23:40 +0530

    Day9 - HTML Inputs & Helloworld.py
    Today worked on HTML inputs, Python setup
    Writter Helloworld python program

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+def hello():
+    print("Hello World")
+    if 1 > 2:
+        print("1 is greater than 2")
+        '''a=1'''
+        #b=2
+    elif 2 > 1:
+        print("1 is not greater than 2")
+    else:
+        print("1 is equal to 2")
+
+hello()
